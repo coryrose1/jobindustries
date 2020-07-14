8d445ce0b5063b97ec753ba21f7580739224e287
Cory Rosenwald <crosenwald@idronline.com>
2020-07-14 17:15:25 -0400

    Adding Nova Industry resource. Adding job_title field to Nova User resource. Creating belongsToMany relationship between the two Nova resources

--- app/Nova/Industry.php
+++ app/Nova/Industry.php
@@ -4,19 +4,18 @@ namespace App\Nova;
 
 use Illuminate\Http\Request;
 use Laravel\Nova\Fields\BelongsToMany;
-use Laravel\Nova\Fields\Gravatar;
 use Laravel\Nova\Fields\ID;
-use Laravel\Nova\Fields\Password;
 use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Http\Requests\NovaRequest;
 
-class User extends Resource
+class Industry extends Resource
 {
     /**
      * The model the resource corresponds to.
      *
      * @var string
      */
-    public static $model = \App\User::class;
+    public static $model = \App\Industry::class;
 
     /**
      * The single value that should be used to represent the resource when being displayed.
@@ -31,7 +30,7 @@ class User extends Resource
      * @var array
      */
     public static $search = [
-        'id', 'name', 'email', 'job_title',
+        'id', 'name',
     ];
 
     /**
@@ -45,28 +44,13 @@ class User extends Resource
         return [
             ID::make()->sortable(),
 
-            Gravatar::make()->maxWidth(50),
-
             Text::make('Name')
                 ->sortable()
-                ->rules('required', 'max:255'),
+                ->rules('required', 'string', 'max:254')
+                ->creationRules('unique:industries,name')
+                ->updateRules('unique:industries,name,{{resourceId}}'),
 
-            Text::make('Email')
-                ->sortable()
-                ->rules('required', 'email', 'max:254')
-                ->creationRules('unique:users,email')
-                ->updateRules('unique:users,email,{{resourceId}}'),
-
-            Text::make('Job Title')
-                ->sortable()
-                ->rules('required', 'string', 'max:255'),
-
-            Password::make('Password')
-                ->onlyOnForms()
-                ->creationRules('required', 'string', 'min:8')
-                ->updateRules('nullable', 'string', 'min:8'),
-
-            BelongsToMany::make('Industries'),
+            BelongsToMany::make('Users'),
         ];
     }
 
